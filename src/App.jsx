ab2ea88c11a70d8e7ff24486621cb40145993e38
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-08-21 13:27:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path=":user" element={<GithubUser />}/>
+          
         </Route>
       </Routes>
     </BrowserRouter>
